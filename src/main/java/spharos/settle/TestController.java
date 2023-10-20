521adabc9e1be33918e191a312be83650d140121
DESKTOP-KA9I336\교육생42 <affogato128@gmail.com>
2023-10-20 17:16:47 +0900

    gateway fix

---
+++ src/main/java/spharos/settle/TestController.java
@@ -0,0 +1,14 @@
+package spharos.settle;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/settle")
+public class TestController {
+    @GetMapping("/test")
+    public String testMethod(){
+        return "Settle Service";
+    }
+}
